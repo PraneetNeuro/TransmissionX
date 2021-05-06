4417e2f390922897159a4317587949d43cc13387
Praneet <praneetquantumx@gmail.com>
2021-05-06 18:25:34 +0530

    Initial commit

---
+++ TransmissionX/TransmissionXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransmissionXApp.swift
+//  TransmissionX
+//
+//  Created by Praneet S on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TransmissionXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
